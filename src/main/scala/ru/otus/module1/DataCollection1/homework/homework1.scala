363a58b812a1a8e6205597e1f822e188d2f43e93
Your Name <you@example.com>
2024-06-13 23:43:44 +0300

    hw-2-first-choice

--- src/main/scala/ru/otus/module1/DataCollection1/homework/homework1.scala
+++ src/main/scala/ru/otus/module1/DataCollection1/homework/homework1.scala
@@ -1,20 +1,57 @@
 package ru.otus.module1.DataCollection1.homework
 
+import scala.annotation.tailrec
 import scala.util.Random
-/*
-class BallsExperiment {
 
-  def isFirstBlackSecondWhite(): Boolean = {
-    ???
+class BallsExperiment {
+  private val listOfBalls: List[Int] = List(1, 0, 1, 0, 1, 0)
+
+  private def getTupleBalls: (Int, Int) = {
+    def handleBall(list: List[Int]): (Int, List[Int]) = {
+      val shuffleList = Random.shuffle(list)
+      val ball = shuffleList.head
+      val otherBalls = shuffleList.tail
+      (ball, otherBalls)
+    }
+    val firstHandle = handleBall(listOfBalls)
+    val firstBall = firstHandle._1
+    val remainingBalls = firstHandle._2
+    val secondBall = handleBall(remainingBalls)._1
+    (firstBall, secondBall)
+  }
+
+  def isFirstBlackSecondWhite: Boolean = {
+    /*
+    Вероятность появления белого шара хотя бы один раз за два вынимания
+     */
+    getTupleBalls match {
+      case (_, 1) => true
+      case (1, _) => true
+      case _ => false
+    }
   }
 }
 
+object BallsExperiment {
+  def apply(): BallsExperiment = new BallsExperiment
+}
+
 object BallsTest {
+  def fillListOfExperiments(count: Int): List[BallsExperiment] = {
+    @tailrec
+    def helperDef(count: Int, list: List[BallsExperiment]): List[BallsExperiment] = {
+      count match {
+        case x if x <= 0 => list
+        case _ => helperDef(count - 1, BallsExperiment() :: list)
+      }
+    }
+    helperDef(count, Nil)
+  }
   def main(args: Array[String]): Unit = {
-    val count = 10000
-    val listOfExperiments: List[BallsExperiment] = ???
-    val countOfExperiments = ???
+    val count = 100000
+    val listOfExperiments: List[BallsExperiment] = fillListOfExperiments(count)
+    val countOfExperiments = listOfExperiments.map(_.isFirstBlackSecondWhite)
     val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
     println(countOfPositiveExperiments / count)
   }
-}*/
+}
