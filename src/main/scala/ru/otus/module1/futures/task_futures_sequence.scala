df5fc07b370543e7d65efbddceba4c94ba1c312f
Yakov Sidin <yashasidin333@yandex.ru>
2024-06-17 12:05:02 +0300

    hw-4

--- src/main/scala/ru/otus/module1/futures/task_futures_sequence.scala
+++ src/main/scala/ru/otus/module1/futures/task_futures_sequence.scala
@@ -3,7 +3,9 @@ package ru.otus.module1.futures
 
 import ru.otus.module1.futures.HomeworksUtils.TaskSyntax
 
+import scala.annotation.tailrec
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
 object task_futures_sequence {
 
@@ -21,7 +23,24 @@ object task_futures_sequence {
    * @return асинхронную задачу с кортежом из двух списков
    */
   def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`" ()
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+    @tailrec
+    def getTupleOfResultOptions(f: Future[A]): (Option[A], Option[Throwable]) = if(f.isCompleted) {
+      f.value match {
+        case Some(Success(result)) => (Some(result), None)
+        case Some(Failure(ex)) => (None, Some(ex))
+        case None => (None, None)
+      }
+    } else {
+      getTupleOfResultOptions(f)
+    }
 
+    def createTuple2WithResultsAndErrors(list: List[(Option[A], Option[Throwable])]): (List[A], List[Throwable]) = {
+      val listSuccess = list.map(_._1).filter(_.isDefined).map(_.get)
+      val listFailed = list.map(_._2).filter(_.isDefined).map(_.get)
+      (listSuccess, listFailed)
+    }
+
+    Future(createTuple2WithResultsAndErrors(futures.map(getTupleOfResultOptions)))
+  }
 }
